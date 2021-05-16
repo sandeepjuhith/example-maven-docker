ede013b9d29eac51a50d43527469132e1039cbd2
sandeepp <sandeep.pallapothu@gmail.com>
2021-05-16 17:43:04 +0530

    docker with maven sample

---
+++ example-maven-docker/src/test/java/com/example/maven/docker/ExampleMavenDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleMavenDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
